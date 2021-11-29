7c7478074dd0494ffce41febe74fce2f31d9ce3b
vant110 <vant110@yandex.ru>
2021-11-30 03:51:09 +0400

    Добавьте файлы проекта.

---
+++ MPI/MPI.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+#include "mpi.h"
+
+const bool isDebug = false;
+const int n = 10; // Размер квадратных матриц.
+
+double** malloc_2d(int, int);
+void free_2d(double**);
+double calcElem(double**, double**, int, int);
+
+int main(int argc, char** argv)
+{ 
+    int currProcess, nProcess;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &currProcess);
+    MPI_Comm_size(MPI_COMM_WORLD, &nProcess);
+    const int nRowNewA = n / nProcess;
+    if (currProcess == 0) {
+        std::cout << "Num processes: " << nProcess << std::endl;
+        // Выделение памяти.
+        double** mA = malloc_2d(n, n);
+        double** mB = malloc_2d(n, n);
+        double** mC = malloc_2d(n, n);
+        // Инициализация матриц-множителей.
+        if (isDebug) std::cout << "Matrix-multiplier:" << std::endl;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mA[i][j] = (double)i + j;
+                mB[i][j] = (double)i + j;
+                if (isDebug) std::cout << mA[i][j] << '\t';
+            }
+            if (isDebug) std::cout << std::endl;
+        }
+        // Отправка матриц-множителей другим процессам.
+        double startTime = MPI_Wtime();
+        int startRowNewA;
+        for (int toProcess = 1; toProcess < nProcess; toProcess++) {
+            startRowNewA = (toProcess - 1) * nRowNewA;
+            MPI_Send(&(mA[startRowNewA][0]), nRowNewA * n, MPI_DOUBLE, toProcess, 0, MPI_COMM_WORLD);
+            MPI_Send(&(mB[0][0]), n * n, MPI_DOUBLE, toProcess, 1, MPI_COMM_WORLD);
+        }
+        // Вычисление полосы результирующей матрицы.
+        startRowNewA = (nProcess - 1) * nRowNewA;
+        for (int i = startRowNewA; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mC[i][j] = calcElem(mA, mB, i, j);
+            }
+        }
+        // Освобождение памяти.
+        free_2d(mA);
+        free_2d(mB);
+        // Получение полос результирующей матрицы от других процессов.
+        if (true) {
+            // Получение сообщений по готовности.
+            double** buffC = malloc_2d(nRowNewA, n);
+            for (int i = 1; i < nProcess; i++) {
+                MPI_Recv(&(buffC[0][0]), nRowNewA * n, MPI_DOUBLE, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, &status);
+                startRowNewA = (status.MPI_SOURCE - 1) * nRowNewA;
+                for (int j = 0; j < nRowNewA * n; j++) {
+                    mC[startRowNewA + j / n][j % n] = buffC[j / n][j % n];
+                }
+            }
+            free_2d(buffC);
+        }
+        else {
+            // Получение сообщений по порядку.
+            for (int fromProcess = 1; fromProcess < nProcess; fromProcess++) {
+                startRowNewA = (fromProcess - 1) * nRowNewA;
+                MPI_Recv(&(mC[startRowNewA][0]), nRowNewA * n, MPI_DOUBLE, fromProcess, 2, MPI_COMM_WORLD, &status);
+            }
+        }
+        double endTime = MPI_Wtime();
+        // Вывод результатов.
+        if (isDebug) {
+            std::cout << "The result of multiplying two identical matrices:" << std::endl;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    std::cout << mC[i][j] << '\t';
+                }
+                std::cout << std::endl;
+            }
+        }
+        std::cout << "Duration of the program: "
+            << endTime - startTime
+            << " sec." << std::endl;
+        // Освобождение памяти.
+        free_2d(mC);
+    }
+    else {
+        // Выделение памяти.
+        double** newA = malloc_2d(nRowNewA, n);
+        double** newB = malloc_2d(n, n);
+        double** newC = malloc_2d(nRowNewA, n);
+        // Получение матриц-множителей от главного процесса.
+        MPI_Recv(&(newA[0][0]), nRowNewA * n, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&(newB[0][0]), n * n, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
+        // Вычисление полосы результирующей матрицы.      
+        for (int i = 0; i < nRowNewA; i++) {
+            for (int j = 0; j < n; j++) {
+                newC[i][j] = calcElem(newA, newB, i, j);
+            }
+        }
+        // Отправление полосы результирующей матрицы главному процессу.  
+        MPI_Send(&(newC[0][0]), nRowNewA * n, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+        // Освобождение памяти.
+        free_2d(newA);
+        free_2d(newB);
+        free_2d(newC);
+    }
+    MPI_Finalize();
+}
+
+double** malloc_2d(int nRow, int nColumn) {
+    double* data = (double*)malloc(nRow * nColumn * sizeof(double));
+    double** arr = (double**)malloc(nRow * sizeof(double*));
+    for (int i = 0; i < nRow; i++) {
+        arr[i] = &(data[nColumn * i]);
+    }
+    return arr;
+}
+
+void free_2d(double** arr) {
+    free(arr[0]);
+    free(arr);
+}
+
+double calcElem(double** mA, double** mB, int i, int j) {
+    double sum = 0;
+    for (int k = 0; k < n; k++) {
+        sum += mA[i][k] * mB[k][j];
+    }
+    return sum;
+}
---
+++ OpenMP/OpenMP.cpp
@@ -0,0 +1,148 @@
+﻿#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include <chrono>
+#include <omp.h>
+
+int main()
+{
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+    _setmode(_fileno(stderr), _O_U16TEXT);
+
+    const bool isDebug = false;
+    int n; // Размер матрицы.
+    double* mA;
+    double* vB;
+    double* vX, * vXPrev;
+    double* vD, * vDPrev;
+    double* vG, * vGPrev;
+    double* vTemp;
+
+#pragma region Ввод значений
+    std::wcout << L"Введите размер матриц: ";
+    std::wcin >> n;
+    {
+        int nThread = 8;
+        //std::wcout << L"Введите количество потоков: ";
+        //std::wcin >> nThread;
+        omp_set_dynamic(0);
+        omp_set_num_threads(nThread);
+    }
+#pragma endregion
+#pragma region Выделение памяти
+    mA = new double[n * n];
+    vB = new double[n];
+    vX = new double[n];
+    vD = new double[n];
+    vG = new double[n];
+    vXPrev = new double[n];
+    vDPrev = new double[n];
+    vGPrev = new double[n];
+    vTemp = new double[n];
+#pragma endregion
+#pragma region Инициализация
+    if (isDebug) std::wcout << L"Матрица А:" << std::endl;
+    /*
+    mA[0] = 3;
+    mA[1] = -1;
+    mA[2] = -1;
+    mA[3] = 3;*/
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            mA[i * n + j] = i + j;
+            if (i + j == 0) mA[0] = 2;
+            if (isDebug) std::wcout << mA[i * n + j] << '\t';
+        }
+        if (isDebug) std::wcout << std::endl;
+    }
+    if (isDebug) std::wcout << std::endl;
+    if (isDebug) std::wcout << L"Вектор b:" << std::endl;
+    /*
+    vB[0] = 3;
+    vB[1] = 7;*/
+    for (int i = 0; i < n; i++) {
+        vB[i] = i;
+        if (i == 0) vB[0] = 1;
+        vXPrev[i] = 0;
+        vDPrev[i] = 0;
+        vGPrev[i] = -vB[i];
+        if (isDebug) std::wcout << vB[i] << '\t';
+    }
+    if (isDebug) std::wcout << std::endl << std::endl;
+#pragma endregion
+
+    auto beginTime = std::chrono::steady_clock::now();
+#pragma region Находим решение СЛАУ
+    int iter = 1, maxIter = n + 1;
+    //float accuracy = 0.0001f;
+
+    do {
+        if (iter > 1) {
+            std::swap(vXPrev, vX);
+            std::swap(vGPrev, vG);
+            std::swap(vDPrev, vD);
+        }
+        //compute gradient
+#pragma omp parallel for
+        for (int i = 0; i < n; i++) {
+            vG[i] = -vB[i];
+            for (int j = 0; j < n; j++)
+                vG[i] += mA[i * n + j] * vXPrev[j];
+        }
+        //compute direction
+        double IP1 = 0, IP2 = 0;
+#pragma omp parallel for reduction(+:IP1,IP2)
+        for (int i = 0; i < n; i++) {
+            IP1 += vG[i] * vG[i];
+            IP2 += vGPrev[i] * vGPrev[i];
+        }
+#pragma omp parallel for
+        for (int i = 0; i < n; i++) {
+            vD[i] = -vG[i] + vDPrev[i] * IP1 / IP2;
+        }
+        //compute size step
+        IP1 = 0;
+        IP2 = 0;
+#pragma omp parallel for reduction(+:IP1,IP2)
+        for (int i = 0; i < n; i++) {
+            vTemp[i] = 0;
+            for (int j = 0; j < n; j++) {
+                vTemp[i] += mA[i * n + j] * vD[j];
+            }
+            IP1 += vD[i] * vG[i];
+            IP2 += vD[i] * vTemp[i];
+        }
+        double step = -IP1 / IP2;
+        //compute new approximation
+#pragma omp parallel for
+        for (int i = 0; i < n; i++) {
+            vX[i] = vXPrev[i] + step * vD[i];
+        }
+        iter++;
+        //} while ((Dest(vXPrev, vX, n) > accuracy) && (iter < maxIter));
+    } while (iter < maxIter);
+    if (isDebug) std::wcout << L"Искомый вектор x:" << std::endl;
+    for (int i = 0; i < n; i++) {
+        if (isDebug) std::wcout << vX[i] << '\t';
+    }
+    if (isDebug) std::wcout << std::endl;
+#pragma endregion
+    auto endTime = std::chrono::steady_clock::now();
+    auto elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - beginTime);
+    std::wcout << L"Длительность работы программы: "
+        << elapsed_ns.count() / 1000000000.
+        << L" сек." << std::endl;
+
+#pragma region Освобождение памяти
+    delete[] mA;
+    delete[] vB;
+    delete[] vX;
+    delete[] vD;
+    delete[] vG;
+    delete[] vXPrev;
+    delete[] vDPrev;
+    delete[] vGPrev;
+    delete[] vTemp;
+#pragma endregion
+}
